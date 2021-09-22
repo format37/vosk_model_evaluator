3c373627e8cb3a8285303b7fe1b313450a3fc5a9
alex <format37@gmail.com>
2021-09-22 12:58:36 +0300

    modified:   reporter/reporter.py 	modified:   reporter/requirements.txt

--- reporter/reporter.py
+++ reporter/reporter.py
@@ -4,6 +4,8 @@ import jiwer
 import asyncio
 import websockets
 import json
+from google.cloud import speech_v1p1beta1
+from google.oauth2 import service_account
 
 
 def get_files(path):
@@ -45,32 +47,59 @@ def error(ground_truth, hypothesis):
 
 async def transcribe_vosk(file_path):
     async with websockets.connect(os.environ.get('VOSK_SERVER_DEFAULT', '')) as websocket:
-        wf = open(file_path, "rb")
+        
         phrases = []
-        while True:				
-            data = wf.read(8000)
-            if len(data) == 0:
-                break
-            await websocket.send(data)
-            accept = json.loads(await websocket.recv())					
-            if len(accept)>1 and accept['text'] != '':
-                #accept_start = str(accept['result'][0]['start'])
-                #accept_end = accept['result'][-1:][0]['end']
-                accept_text = str(accept['text'])					
-                phrases.append(accept_text)						
-                #phrases_count += 1
+        with open(file_path, "rb") as audio_file:
+            while True:				
+                data = audio_file.read(8000)
+                if len(data) == 0:
+                    break
+                await websocket.send(data)
+                accept = json.loads(await websocket.recv())					
+                if len(accept)>1 and accept['text'] != '':
+                    accept_text = str(accept['text'])					
+                    phrases.append(accept_text)
 
-        await websocket.send('{"eof" : 1}')
-        accept = json.loads(await websocket.recv())
-        # TODO: merge this cloned section:
-        if len(accept)>1 and accept['text'] != '':
-            #accept_start = str(accept['result'][0]['start'])
-            #accept_end = accept['result'][-1:][0]['end']
-            accept_text = str(accept['text'])            			
-            phrases.append(accept_text)						
-            #phrases_count += 1
+            await websocket.send('{"eof" : 1}')
+            accept = json.loads(await websocket.recv())
+            if len(accept)>1 and accept['text'] != '':
+                accept_text = str(accept['text'])            			
+                phrases.append(accept_text)        
 
     return phrases
+
+
+def transcribe_google(file_path):
+    
+    # https://cloud.google.com/speech-to-text
+    # https://cloud.google.com/speech-to-text/docs/reference/rest/v1/RecognitionConfig?hl=ru#AudioEncoding
+    # https://cloud.google.com/speech-to-text/docs/samples/speech-transcribe-async#speech_transcribe_async-python
+    
+    creds = service_account.Credentials.from_service_account_file(
+        os.environ.get('GOOGLE_CREDENTIALS_FILE_PATH', ''),
+        scopes=['https://www.googleapis.com/auth/cloud-platform']
+        )
+    language = 'ru-RU'
+    client = speech_v1p1beta1.SpeechClient(credentials=creds)
+    sample_rate_hertz = 8000
+    encoding = speech_v1p1beta1.RecognitionConfig.AudioEncoding.MP3
+
+    config = {
+        "language_code": language,
+        "sample_rate_hertz": sample_rate_hertz,
+        "encoding": encoding,
+    }
+    #with io.open(file_path, 'rb') as audio_file:
+    with open(file_path, "rb") as audio_file:
+        content = audio_file.read()
+        audio = speech_v1p1beta1.RecognitionAudio(content=content)
+
+        response = client.recognize(config = config, audio = audio)
+        results = []
+        for result in response.results:
+            alternative = result.alternatives[0]
+            results.append(alternative.transcript)
+    return ''.join([str(item) for item in results]).lower()
 
 
 def main():
@@ -81,12 +110,19 @@ def main():
     evals_wil = [] 
 
 
-    print('files:')
+    print('vosk files:')
     for filename in files:
         print(path + filename)
         phrases = asyncio.get_event_loop().run_until_complete(transcribe_vosk(path + filename))
-        for phrase in phrases:
-            print(phrase)
+        text_vosk = ' '.join(phrases).replace('  ',' ')
+        print(text_vosk)
+
+    print('google files:')
+    for filename in files:
+        print(path + filename)        
+        text_google = transcribe_google(path + filename).replace('  ',' ')
+        print(text_vosk)
+        
 
 
 if __name__ == "__main__":
